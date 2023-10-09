c5af70736e65b48232f63ee2429e6372306fc8bc
hsilv <hm.sebastiansilva@gmail.com>
2023-10-08 19:45:08 -0600

    estrellas

--- src/pages/IndexPage/IndexPage.jsx
+++ src/pages/IndexPage/IndexPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import WelcomePage from '../WelcomePage';
+import { EclipseAnimation } from '../../components/eclipseAnimation/eclipseAnimation';
 
 const IndexPage = () => (
     <Routes>
-        <Route path="/" element={<WelcomePage />} />
+        <Route path="/" element={<EclipseAnimation />} />
     </Routes>
 )
 
