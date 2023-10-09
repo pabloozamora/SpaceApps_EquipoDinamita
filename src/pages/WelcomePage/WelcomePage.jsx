dd5f62f0202b82ae3fb1cac5b1989a6d7d8aa638
Diego <diegoguatedb2002@gmail.com>
2023-10-08 21:50:09 -0600

    Welcome page avances

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React from 'react';
 import styles from './WelcomePage.module.css';
+import solarImg from '../../assets/solar2.png';
+import { Link } from 'react-router-dom';
 
 function WelcomePage() {
-    return(<div className={styles.welcomePage}>
-        Hola
-    </div>)
+  return (
+    <div className={styles.welcomePage}>
+        <nav className={styles.topBar}>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/page1">Page 1</Link>
+                </li>
+                <li>
+                    <Link to="/page2">Page 2</Link>
+                </li>
+            </ul>
+        </nav>
+      <div className={styles.welcomeBanner}>
+          <header className={styles.titleContainer}>
+            <h1>Eclipses</h1>
+            <h2>Perspective is everything</h2>
+          </header>
+          <img src={solarImg} className={styles.solarImage} />
+
+      </div>
+    </div>
+  );
 }
 
 export default WelcomePage;
